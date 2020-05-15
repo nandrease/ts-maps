9b57240f595485ba9dc0b02d0325502b38a263fe
nandrease <neeme.andreas.eller@gmail.com>
2020-05-15 20:42:43 +0300

    Add User, Company and CustomMap classes

---
+++ src/CustomMap.ts
@@ -0,0 +1,31 @@
+import { User } from './User';
+import { Company } from './Company';
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      backgroundColor: 'goldenrod',
+      center: { lat: 45, lng: 45 }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hi there")
+import { CustomMap } from './CustomMap';
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
